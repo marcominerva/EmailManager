c0e757571553198f2dc28e1f504d6574655d1a91
Marco Minerva <marco.minerva@gmail.com>
2025-06-27 14:33:16 +0200

    Add localization and rate limiting to email service
    Updated `Program.cs` to include localization and rate limiting services. Added a "SendEmail" rate limiter policy and configured request localization with "en-US" as the default culture. Modified OpenAPI registration to enhance problem details and accept language headers. Updated middleware pipeline to include routing, localization, and rate limiting.
    
    Introduced new variables in `EmailManager.http` for host address and API key, along with a sample POST request for sending emails, including sender details and content in both HTML and plain text formats.

--- src/EmailManager/Program.cs
+++ src/EmailManager/Program.cs
@@ -1,10 +1,14 @@
+using System.Globalization;
+using System.Threading.RateLimiting;
 using EmailManager.BusinessLayer.Services;
 using EmailManager.BusinessLayer.Services.Interfaces;
 using EmailManager.BusinessLayer.Validations;
 using EmailManager.Shared.Models;
 using FluentValidation;
+using Microsoft.AspNetCore.Localization;
 using MinimalHelpers.FluentValidation;
 using TinyHelpers.AspNetCore.Extensions;
+using TinyHelpers.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +16,54 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddKeyedSingleton<IEmailService, SmtpEmailService>("smtp");
 //builder.Services.AddKeyedSingleton<IEmailService, SendgridEmailService>("sendgrid");
 
+builder.Services.AddRequestLocalization(options =>
+{
+    var supportedCultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("SendEmail", context =>
+    {
+        var permitLimit = int.TryParse(context.User.Claims.FirstOrDefault(c => c.Type == "requests_per_window")?.Value, out var requestPerWindow) ? requestPerWindow : 3;
+        var window = int.TryParse(context.User.Claims.FirstOrDefault(c => c.Type == "window_minutes")?.Value, out var windowMinutes) ? TimeSpan.FromMinutes(windowMinutes) : TimeSpan.FromMinutes(1);
+
+        return RateLimitPartition.GetFixedWindowLimiter(context.User.Identity?.Name ?? "Default", _ => new FixedWindowRateLimiterOptions
+        {
+            PermitLimit = permitLimit,
+            Window = window,
+            QueueLimit = 0,
+            QueueProcessingOrder = QueueProcessingOrder.OldestFirst
+        });
+    });
+
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    options.OnRejected = (context, _) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var window))
+        {
+            var response = context.HttpContext.Response;
+            response.Headers.RetryAfter = window.TotalSeconds.ToString();
+        }
+
+        return ValueTask.CompletedTask;
+    };
+});
+
+ValidatorOptions.Global.LanguageManager.Enabled = false;
 builder.Services.AddValidatorsFromAssemblyContaining<EmailMessageValidator>();
 
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.RemoveServerList();
+
+    options.AddDefaultProblemDetailsResponse();
+    options.AddAcceptLanguageHeader();
+});
 
 builder.Services.AddDefaultProblemDetails();
 builder.Services.AddDefaultExceptionHandler();
@@ -33,6 +82,16 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/openapi/v1.json", app.Environment.ApplicationName);
 });
 
+app.UseRouting();
+//app.UseCors();
+
+app.UseRequestLocalization();
+
+//app.UseAuthentication();
+//app.UseAuthorization();
+
+app.UseRateLimiter();
+
 app.MapPost("/api/{service:regex(smtp)}", async (string service, IServiceProvider serviceProvider, EmailMessage emailMessage, CancellationToken cancellationToken) =>
 {
     var emailService = serviceProvider.GetRequiredKeyedService<IEmailService>(service);
@@ -41,6 +100,7 @@ app.MapPost("/api/{service:regex(smtp)}", async (string service, IServiceProvide
     return TypedResults.Ok(response);
 })
 .Produces<SendEmailResult>()
-.WithValidation<EmailMessage>();
+.WithValidation<EmailMessage>()
+.RequireRateLimiting("SendEmail");
 
 app.Run();
